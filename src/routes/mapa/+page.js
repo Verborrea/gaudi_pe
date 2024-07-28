a0322a05cba532f30c3192bf7b669920733d44e6
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2024-07-28 17:55:28 -0500

    ahhh

--- src/routes/mapa/+page.js
+++ src/routes/mapa/+page.js
@@ -12,50 +12,7 @@ export async function load({ fetch }) {
 
 	let zonas = await res.json();
 
-	zonas =
-`
-	{
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-	    "id": 1,
-	  	"precio": 5,
-	  	"dias": ["Lunes", "Martes", "Sábado"]
-	  },
-      "geometry": {
-        "coordinates": [
-          [
-            [
-              -71.58427137162141,
-              -16.378648880859657
-            ],
-            [
-              -71.58427137162141,
-              -16.448732409408876
-            ],
-            [
-              -71.4785889974782,
-              -16.448732409408876
-            ],
-            [
-              -71.4785889974782,
-              -16.378648880859657
-            ],
-            [
-              -71.58427137162141,
-              -16.378648880859657
-            ]
-          ]
-        ],
-        "type": "Polygon"
-      }
-    }
-  ]
-}
-`
-	// zonas = `{"type": "FeatureCollection","features": ${JSON.stringify(zonas)}}`
-	
+	zonas = `{"type": "FeatureCollection","features": ${zonas}}`
+
 	return { zonas }
 }
